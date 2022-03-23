9ba6ba6b7f1c25b0842fd5e5626d5190ab4227db
j4ck <cacodemon1123@gmail.com>
2022-03-23 09:55:10 +0200

    Working MessageBox function

--- src/main/java/com/j4ce/sillylang/base/SillyCode.java
+++ src/main/java/com/j4ce/sillylang/base/SillyCode.java
@@ -1,5 +1,6 @@
 package com.j4ce.sillylang.base;
 
+import com.j4ce.sillylang.methods.OutputMessageBoxMethod;
 import com.j4ce.sillylang.methods.SetGlobalVarMethod;
 import com.j4ce.sillylang.methods.WriteConsoleMethod;
 import com.j4ce.sillylang.statements.ForLoopStatement;
@@ -32,7 +33,8 @@ public class SillyCode {
                 setGlobalVarMethod.run();
             }
             case M_OutputMessageBox: {
-
+                OutputMessageBoxMethod outputMessageBoxMethod = new OutputMessageBoxMethod(node);
+                outputMessageBoxMethod.run();
                 break;
             }
             default: {
--- src/main/java/com/j4ce/sillylang/methods/OutputMessageBoxMethod.java
+++ src/main/java/com/j4ce/sillylang/methods/OutputMessageBoxMethod.java
@@ -4,6 +4,8 @@ import com.j4ce.sillylang.base.SillyException;
 import com.j4ce.sillylang.util.Attributes;
 import org.w3c.dom.Node;
 
+import javax.swing.*;
+
 public class OutputMessageBoxMethod extends Method {
     Node node;
 
@@ -12,16 +14,22 @@ public class OutputMessageBoxMethod extends Method {
     }
     @Override
     public void run() {
-        String title;
-        String text;
+        String title = null;
+        String text = null;
 
         try {
             title = Attributes.GetAttributeValue(node, "title");
-            text = Attributes.GetAttributeValue(node, "text");
         } catch (NullPointerException e) {
-            SillyException.ThrowWithLocation(node, "You must set the title name.");
+            SillyException.ThrowWithLocation(node, "You must set the messagebox title.");
             System.exit(1);
         }
+        try {
+            text = Attributes.GetAttributeValue(node, "text");
+        } catch (NullPointerException e) {
+            SillyException.ThrowWithLocation(node, "You must set the messagebox text.");
+            System.exit(1);
+        }
+        JOptionPane.showMessageDialog(null, text, title, JOptionPane.INFORMATION_MESSAGE);
 
 
     }
